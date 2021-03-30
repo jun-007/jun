617b3b42320e45bb14455e9aaf9e112230cd6d7f
jun-007 <78342375+jun-007@users.noreply.github.com>
2021-03-30 23:27:15 +0800

    Add files via upload

---
+++ 2+22+222.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int n = 0;
+	scanf("%d%d", &a, &n);
+	int sum = 0;
+	int i = 9;
+	int ret = 0;
+	//2+22+222+2222
+	for (i = 0; i < n; i++)
+	{
+		ret = ret * 10 + a;
+		sum += ret;
+
+	}
+	printf("%d\n", sum);
+	return 0;
+}
