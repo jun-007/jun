6795077e65284b4a400afe772d1417ea04a5f253
jun-007 <78342375+jun-007@users.noreply.github.com>
2021-04-02 11:27:08 +0800

    Add files via upload

---
+++ 菱形.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int line = 0;
+	scanf("%d", &line);
+	//打印上半部分
+	int i = 0;
+	for (i = 0; i < line; i++)
+	{
+		//打印空格
+		int j = 0;
+		for (j = 0; j <line-1-i ; j++)
+		{
+			printf(" ");
+		}
+		//打印星
+		for (j = 0; j <2*i+1 ; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+		
+	}
+	//打印下部分
+	for (i = 0; i <line-1; i++)
+	{
+		int j = 0;
+		for (j = 0; j <= i; j++)
+		{
+			printf(" ");
+		}
+		//打印星
+		for (j = 0; j <2*(line-i-1)-1 ; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
